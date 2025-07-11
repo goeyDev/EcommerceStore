29c8592e696ca71f4488fa549543c68565fc851b
goeyDev <chgoey@outloog.com>
2025-07-11 18:11:16 +0800

    return the origin db driver

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,9 +1,9 @@
-// import { neon } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-http"; // Use Neon-specific driver
-// import * as schema from "./schema";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http"; // Use Neon-specific driver
+import * as schema from "./schema";
 
-// const sql = neon(process.env.DATABASE_URL!);
-// export const db = drizzle(sql, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
 // online
 
 // local
@@ -12,13 +12,14 @@
 
 // export const db = drizzle(process.env.DATABASE_URL!, { schema });
 // local
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres"; // not neon-http
-import * as schema from "./schema";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+// import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres"; // not neon-http
+// import * as schema from "./schema";
 
-export const db = drizzle(pool, { schema });
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
+
+// export const db = drizzle(pool, { schema });
