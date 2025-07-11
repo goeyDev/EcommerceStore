f92db8af5090dcfe51dab962e034236cb58b2933
goeyDev <chgoey@outloog.com>
2025-07-11 15:47:16 +0800

    add consolelog on main page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -18,7 +18,7 @@ export default async function AdminDashboard() {
     getUserData(),
     getProductData(),
   ]);
-
+  console.log("test");
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
       <DashboardCard
