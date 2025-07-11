4e43873199fce78f7931bb28ad8f344f8747cb37
goeyDev <chgoey@outloog.com>
2025-07-11 21:48:26 +0800

    return old db and mv tripe out of api

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,9 +1,9 @@
-// import { neon } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-http"; // Use Neon-specific driver
-// import * as schema from "./schema";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http"; // Use Neon-specific driver
+import * as schema from "./schema";
 
-// const sql = neon(process.env.DATABASE_URL!);
-// export const db = drizzle(sql, { schema });
+const sql = neon(process.env.DATABASE_URL!);
+export const db = drizzle(sql, { schema });
 // online
 
 // local
@@ -13,13 +13,13 @@
 // export const db = drizzle(process.env.DATABASE_URL!, { schema });
 // local
 
-import { Pool } from "pg";
-import { drizzle } from "drizzle-orm/node-postgres"; // not neon-http
-import * as schema from "./schema";
+// import { Pool } from "pg";
+// import { drizzle } from "drizzle-orm/node-postgres"; // not neon-http
+// import * as schema from "./schema";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-});
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// });
 
-export const db = drizzle(pool, { schema });
+// export const db = drizzle(pool, { schema });
