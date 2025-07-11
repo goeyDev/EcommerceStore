55d6c8baf809cb2d7f47bc986d126a8b93df3aab
goeyDev <chgoey@outloog.com>
2025-07-11 15:16:52 +0800

    delete consolelog page

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -19,9 +19,6 @@ export default async function AdminDashboard() {
     getProductData(),
   ]);
 
-  userData;
-
-  ("salesData", salesData);
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
       <DashboardCard
