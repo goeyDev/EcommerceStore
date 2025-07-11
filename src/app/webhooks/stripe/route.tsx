c63322bd2dff4cfb7647ebe1bf3b8cad3e72cb62
goeyDev <chgoey@outloog.com>
2025-07-11 18:00:02 +0800

    add trycatch on route

--- src/app/webhooks/stripe/route.tsx
+++ src/app/webhooks/stripe/route.tsx
@@ -20,69 +20,74 @@ export async function POST(req: NextRequest) {
     process.env.STRIPE_WEBHOOK_SECRET as string
   );
 
-  if (event.type === "charge.succeeded") {
-    const charge = event.data.object;
-    const productId = charge.metadata.productId;
-    const email = charge.billing_details.email;
-    const pricePaidInCents = charge.amount;
-    const userId = charge.metadata.loggedInId;
+  try {
+    if (event.type === "charge.succeeded") {
+      const charge = event.data.object;
+      const productId = charge.metadata.productId;
+      const email = charge.billing_details.email;
+      const pricePaidInCents = charge.amount;
+      const userId = charge.metadata.loggedInId;
 
-    // const product = await db.product.findUnique({ where: { id: productId } })
-    const product = await db.query.productsTable.findFirst({
-      where: eq(productsTable.id, productId),
-    });
-    if (product == null || email == null) {
-      return new NextResponse("Bad Request", { status: 400 });
+      // const product = await db.product.findUnique({ where: { id: productId } })
+      const product = await db.query.productsTable.findFirst({
+        where: eq(productsTable.id, productId),
+      });
+      if (product == null || email == null) {
+        return new NextResponse("Bad Request", { status: 400 });
+      }
+
+      // const userId = product.userId;
+      // console.log("userId from webhooks", userId);
+
+      // ✅ 1. Insert new order for logged-in user
+      await db.insert(ordersTable).values({
+        userId, // already authenticated,from matadata,purchase page
+        productId,
+        pricePaidInCents,
+      });
+
+      console.log("ordersTable inserted");
+
+      // ✅ 2. Fetch latest order for this user
+      const [order] = await db
+        .select()
+        .from(ordersTable)
+        .where(eq(ordersTable.userId, userId))
+        .orderBy(desc(ordersTable.createdAt))
+        .limit(1);
+
+      console.log("ordersTable calling");
+
+      // Calculate expiry time (24 hours from now)
+      const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24);
+
+      const [downloadVerification] = await db
+        .insert(downloadVerificationTable)
+        .values({
+          productId,
+          expiresAt,
+        })
+        .returning();
+
+      await resend.emails.send({
+        from: `Support <${process.env.SENDER_EMAIL}>`,
+        to: email,
+        subject: "Order Confirmation",
+        react: (
+          <PurchaseReceiptEmail
+            order={order}
+            product={product}
+            downloadVerificationId={downloadVerification.id}
+          />
+        ),
+      });
     }
 
-    // const userId = product.userId;
-    // console.log("userId from webhooks", userId);
-
-    // ✅ 1. Insert new order for logged-in user
-    await db.insert(ordersTable).values({
-      userId, // already authenticated,from matadata,purchase page
-      productId,
-      pricePaidInCents,
-    });
-
-    console.log("ordersTable inserted");
-
-    // ✅ 2. Fetch latest order for this user
-    const [order] = await db
-      .select()
-      .from(ordersTable)
-      .where(eq(ordersTable.userId, userId))
-      .orderBy(desc(ordersTable.createdAt))
-      .limit(1);
-
-    console.log("ordersTable calling");
-
-    // Calculate expiry time (24 hours from now)
-    const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24);
-
-    const [downloadVerification] = await db
-      .insert(downloadVerificationTable)
-      .values({
-        productId,
-        expiresAt,
-      })
-      .returning();
-
-    await resend.emails.send({
-      from: `Support <${process.env.SENDER_EMAIL}>`,
-      to: email,
-      subject: "Order Confirmation",
-      react: (
-        <PurchaseReceiptEmail
-          order={order}
-          product={product}
-          downloadVerificationId={downloadVerification.id}
-        />
-      ),
-    });
+    return new NextResponse();
+  } catch (err) {
+    console.error("❌ Webhook error:", err);
+    return new NextResponse("Webhook error", { status: 500 });
   }
-
-  return new NextResponse();
 }
 
 // return inserted row(s)
